ece76a40b05cb6d55c654106c43b16fb0d8a7989
balsimpson <thirdi@gmail.com>
2016-06-29 16:22:28 +0530

    basic things to do

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # eden
 an automated gardening system
+
+
+check and log
+- temperature
+- light level
+- soil moisture level
---
+++ eden.ino
@@ -0,0 +1,11 @@
+// declare variables
+
+// check soil moisture level
+// log
+
+// check light level
+// log
+
+// tweet
+// - if thirsting
+// - when watered
