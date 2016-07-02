8f811335d92e7da78cf9aa86fcdd7ab49493f828
balsimpson <thirdi@gmail.com>
2016-07-02 15:36:07 +0530

    added thinger.io basic config

--- eden.ino
+++ eden.ino
@@ -1,18 +1,36 @@
-// declare variables
+// libraries
+#define _DEBUG_
+#include <ESP8266WiFi.h>
+#include <ThingerWifi.h>
 // adafruit
 // blynk
+
 // thinger.io
+#define USERNAME "balsimpson"
+#define DEVICE_ID "DEVICE_ID"
+#define DEVICE_CREDENTIAL "DEVICE_CREDENTIAL"
+
+#define SSID "SSID"
+#define SSID_PASSWORD "SSID_PASSWORD"
 
 // millis
 unsigned long previousMillis = 0;
 // 5 minutes
 const long interval = 300000;
 
+// initialize thinger.io
+ThingerWifi thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
 void setup() {
+  Serial.begin(115200);
   unsigned long currentMillis = millis();
+  // thinger.io
+  thing.add_wifi(SSID, SSID_PASSWORD);
 }
 
-void draw() {
+void loop() {
+  // thinger.io
+  thing.handle();
   // check & log sensor data
   sensorCheck();
   if moisture < optimum
