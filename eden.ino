5fc5b6cf19c764897ddeadda3a1e8691532018ae
balsimpson <thirdi@gmail.com>
2016-07-02 15:30:03 +0530

    updated

--- eden.ino
+++ eden.ino
@@ -1,11 +1,40 @@
 // declare variables
+// adafruit
+// blynk
+// thinger.io
 
-// check soil moisture level
-// log
+// millis
+unsigned long previousMillis = 0;
+// 5 minutes
+const long interval = 300000;
 
-// check light level
-// log
+void setup() {
+  unsigned long currentMillis = millis();
+}
 
-// tweet
-// - if thirsting
-// - when watered
+void draw() {
+  // check & log sensor data
+  sensorCheck();
+  if moisture < optimum
+  edenTweet();
+}
+
+void sensorCheck() {
+  if (currentMillis - previousMillis >= interval) {
+    // check soil moisture level
+    // check light level
+    sensorLog();
+    previousMillis = currentMillis;
+  }
+}
+
+void sensorLog() {
+  log moisture level
+  log light level
+}
+
+void edenTweet() {
+  // tweet
+  // - if thirsting
+  // - when watered
+}
